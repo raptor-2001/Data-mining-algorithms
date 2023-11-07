056259a58c66fb648f819e8d7a7823ae7ab42a78
raptor-2001 <pratham007yadav@gmail.com>
2023-11-07 19:13:31 +0530

    exp6 changed

--- exp_6/exp6.cpp
+++ exp_6/exp6.cpp
@@ -1,13 +1,69 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
 #include <vector>
+#include <string>
+#include <sstream>
+#include <climits>
+#include <cmath>
 #include <algorithm>
 
 using namespace std;
 
+// Function to read CSV data from a file and return it as a vector of vectors of strings
+vector<vector<string>> readCSV(const string &filename)
+{
+    vector<vector<string>> data;
+    ifstream file(filename);
+    if (!file.is_open())
+    {
+        cerr << "Error: File " << filename << " not found." << endl;
+        return data;
+    }
+
+    string line;
+    while (getline(file, line))
+    {
+        vector<string> row;
+        istringstream lineStream(line);
+        string cell;
+        while (getline(lineStream, cell, ','))
+        {
+            row.push_back(cell);
+        }
+        data.push_back(row);
+    }
+
+    file.close();
+    return data;
+}
+
+// Function to write CSV data to a file
+void writeCSV(const string &filename, const vector<vector<string>> &data)
+{
+    ofstream file(filename);
+    if (!file.is_open())
+    {
+        cerr << "Error: Unable to create or open file " << filename << " for writing." << endl;
+        return;
+    }
+
+    for (const auto &row : data)
+    {
+        for (size_t i = 0; i < row.size(); ++i)
+        {
+            file << row[i];
+            if (i < row.size() - 1)
+            {
+                file << ",";
+            }
+        }
+        file << endl;
+    }
+
+    file.close();
+}
 // Function to calculate the median of a vector
-float calculateMedian(vector<int> a)
+double calculateMedian(vector<int> a)
 {
     int size = a.size();
     if (size % 2 == 1)
@@ -17,7 +73,7 @@ float calculateMedian(vector<int> a)
 }
 
 // Function to calculate the first quartile (Q1)
-float calculateQuartile1(vector<int> v)
+double calculateQuartile1(vector<int> v)
 {
     int n = v.size();
     vector<int> first;
@@ -30,7 +86,7 @@ float calculateQuartile1(vector<int> v)
 }
 
 // Function to calculate the third quartile (Q3)
-float calculateQuartile3(vector<int> v)
+double calculateQuartile3(vector<int> v)
 {
     int n = v.size();
     vector<int> last;
@@ -53,54 +109,45 @@ float calculateQuartile3(vector<int> v)
 
 int main()
 {
-    ifstream in("five_number_input.csv");
-    if (!in.is_open())
-    {
-        cout << "Error: Unable to open the input file." << endl;
-        exit(0);
-    }
-
-    ofstream out("five_number_output.csv");
+    vector<vector<string>> marks = readCSV("five_number_input.csv");
 
     int i = 0;
     string line, mark;
-    vector<int> arr;
+    vector<int> buffer;
 
-    // Read data from the input file
-    while (getline(in, line))
+    for (auto ele : marks[0])
     {
-        if (i == 0)
-        {
-            i++;
-            continue;
-        }
-        stringstream str(line);
-
-        getline(str, mark, ',');
-        int x = stoi(mark);
-        arr.push_back(x);
+        int x = stoi(ele);
+        buffer.push_back(x);
     }
 
-    int n = arr.size();
-    sort(arr.begin(), arr.end());
+    int n = buffer.size();
+    sort(buffer.begin(), buffer.end());
 
-    // Write results to the output file and console
-    out << "Minimum value: "
-        << "," << arr[0] << "\n";
-    out << "First Quartile (Q1) value: "
-        << "," << calculateQuartile1(arr) << "\n";
-    out << "Median value: "
-        << "," << calculateMedian(arr) << "\n";
-    out << "Third Quartile (Q3) value: "
-        << "," << calculateQuartile3(arr) << "\n";
-    out << "Maximum value: "
-        << "," << arr[n - 1] << "\n";
+    vector<vector<string>> fiveNumbers;
 
-    cout << "The minimum value is " << arr[0] << endl;
-    cout << "The First Quartile (Q1) is " << calculateQuartile1(arr) << endl;
-    cout << "The median is " << calculateMedian(arr) << endl;
-    cout << "The Third Quartile (Q3) is " << calculateQuartile3(arr) << endl;
-    cout << "The maximum value is " << arr[n - 1] << endl;
+    vector<string> minimumValue;
+    minimumValue.push_back("min: " + to_string(buffer[0]));
+    fiveNumbers.push_back(minimumValue);
+
+    vector<string> q1Value;
+    q1Value.push_back("Q1: " + to_string(calculateQuartile1(buffer)));
+    fiveNumbers.push_back(q1Value);
+
+    vector<string> medValue;
+    medValue.push_back("med: " + to_string(calculateMedian(buffer)));
+    fiveNumbers.push_back(medValue);
+
+    vector<string> q3Value;
+    q3Value.push_back("Q3: " + to_string(calculateQuartile3(buffer)));
+    fiveNumbers.push_back(q3Value);
+
+    vector<string> maximumValue;
+    maximumValue.push_back("max: " + to_string(buffer[n - 1]));
+    fiveNumbers.push_back(maximumValue);
+
+    writeCSV("five_number_output.csv", fiveNumbers);
+    cout << "Five numbers calculated successfully." << endl;
 
     return 0;
 }
---
+++ exp_6/tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+
+#include <vector>
+#include <string>
+#include <sstream>
+#include <climits>
+#include <cmath>
