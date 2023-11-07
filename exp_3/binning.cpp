b0e420d4f0e6a3e018f9fc8e60b0555c38af66ba
raptor-2001 <pratham007yadav@gmail.com>
2023-11-07 11:02:52 +0530

    exp_3 added

--- exp_3/binning.cpp
+++ exp_3/binning.cpp
@@ -1,134 +1,203 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
 #include <sstream>
 #include <climits>
+#include <cmath>
+#include <algorithm>
 
 using namespace std;
 
-// Equal Frequency Binning
-vector<vector<int>> equifreq(vector<int> arr1, int m)
+// Function to read CSV data from a file and return it as a vector of vectors of strings
+vector<vector<string>> readCSV(const string &filename)
 {
-  int a = arr1.size();
-  int n = a / m;
-  vector<vector<int>> bins;
-  for (int i = 0; i < m; i++)
+  vector<vector<string>> data;
+  ifstream file(filename);
+  if (!file.is_open())
   {
-    vector<int> bin;
-    for (int j = i * n; j < (i + 1) * n; j++)
-    {
-      if (j >= a)
-      {
-        break;
-      }
-      bin.push_back(arr1[j]);
-    }
-    bins.push_back(bin);
+    cerr << "Error: File " << filename << " not found." << endl;
+    return data;
   }
-  return bins;
+
+  string line;
+  while (getline(file, line))
+  {
+    vector<string> row;
+    istringstream lineStream(line);
+    string cell;
+    while (getline(lineStream, cell, ','))
+    {
+      row.push_back(cell);
+    }
+    data.push_back(row);
+  }
+
+  file.close();
+  return data;
 }
 
-// Equal Width Binning
-vector<vector<int>> equiwidth(vector<int> arr1, int m)
+// Function to write CSV data to a file
+void writeCSV(const string &filename, const vector<vector<string>> &data)
 {
-  int a = arr1.size();
-
-  int max_ele = INT_MIN;
-  int min_ele = INT_MAX;
-
-  for (int i = 0; i < arr1.size(); i++)
+  ofstream file(filename);
+  if (!file.is_open())
   {
-    max_ele = max(max_ele, arr1[i]);
-    min_ele = min(min_ele, arr1[i]);
+    cerr << "Error: Unable to create or open file " << filename << " for writing." << endl;
+    return;
   }
 
-  int w = (max_ele - min_ele) / m;
-  int min1 = min_ele;
-
-  vector<int> arr;
-  for (int i = 0; i < m + 1; i++)
+  for (const auto &row : data)
   {
-    arr.push_back(min1 + w * i);
-  }
-
-  vector<vector<int>> arri;
-  for (int i = 0; i < m; i++)
-  {
-    vector<int> temp;
-    for (int j : arr1)
+    for (size_t i = 0; i < row.size(); ++i)
     {
-      if (j >= arr[i] && j <= arr[i + 1])
+      file << row[i];
+      if (i < row.size() - 1)
+      {
+        file << ",";
+      }
+    }
+    file << endl;
+  }
+
+  file.close();
+}
+
+void equiwidthBinning()
+{
+  const vector<vector<string>> data = readCSV("input.csv");
+  vector<string> row = data[0]; // Extract the single row of data
+
+  int num_bins;
+  double min_val = INT_MAX;
+  double max_val = INT_MIN;
+
+  cout << "Enter the number of bins: ";
+  cin >> num_bins;
+
+  // Find the minimum and maximum values in the data
+  for (const string &cell : row)
+  {
+    double value = stod(cell);
+    if (value < min_val)
+    {
+      min_val = value;
+    }
+    if (value > max_val)
+    {
+      max_val = value;
+    }
+  }
+
+  // Calculate the bin width
+  double bin_width = (max_val - min_val) / num_bins;
+  double min1 = min_val;
+
+  vector<double> bin_edges;
+
+  for (int i = 0; i < num_bins + 1; i++)
+  {
+    bin_edges.push_back(min1 + i * bin_width);
+  }
+
+  vector<vector<string>> new_binned_data;
+
+  // Binning the data
+  for (int i = 0; i < num_bins; i++)
+  {
+    vector<string> temp;
+    for (const string &j : row)
+    {
+      double ele = stod(j);
+      if (ele >= bin_edges[i] && ele <= bin_edges[i + 1])
       {
         temp.push_back(j);
       }
     }
-    arri.push_back(temp);
+    new_binned_data.push_back(temp);
   }
-  return arri;
+
+  // Write the binned data to a file
+  writeCSV("output_equiwidth.csv", new_binned_data);
+
+  cout << "Equiwidth binning complete." << endl;
 }
 
-// Read data from CSV
-vector<int> readCSV(string filename)
+void equiFrequencyBinning()
 {
-  ifstream inputFile(filename);
-  vector<int> data;
-  string line, value;
-  while (getline(inputFile, line))
-  {
-    stringstream ss(line);
-    while (getline(ss, value, ','))
-    {
-      data.push_back(stoi(value));
-    }
-  }
-  inputFile.close();
-  return data;
-}
+  const vector<vector<string>> data = readCSV("input.csv");
+  vector<string> row = data[0]; // Extract the single row of data
 
-// Write binning outputs to CSV
-void writeCSV(string filename, vector<vector<int>> bins)
-{
-  ofstream outputFile(filename);
-  for (int i = 0; i < bins.size(); i++)
+  int num_bins;
+  cout << "Enter the number of bins: ";
+  cin >> num_bins;
+
+  // Sort the data in ascending order
+  vector<double> sorted_data;
+  for (const string &cell : row)
   {
-    outputFile << "Bin " << i + 1 << ",";
-    for (int num : bins[i])
-    {
-      outputFile << num << ",";
-    }
-    outputFile << "\n";
+    sorted_data.push_back(stod(cell));
   }
-  outputFile.close();
+  sort(sorted_data.begin(), sorted_data.end());
+
+  int data_size = sorted_data.size();
+  int items_per_bin = data_size / num_bins;
+  int remaining_items = data_size % num_bins;
+
+  vector<vector<string>> new_binned_data;
+
+  // Binning the data
+  int start_index = 0;
+  for (int i = 0; i < num_bins; i++)
+  {
+    vector<string> temp;
+    int items_in_bin = items_per_bin;
+
+    // Distribute any remaining items to the first bins
+    if (remaining_items > 0)
+    {
+      items_in_bin++;
+      remaining_items--;
+    }
+
+    for (int j = 0; j < items_in_bin; j++)
+    {
+      temp.push_back(to_string(sorted_data[start_index + j]));
+    }
+    start_index += items_in_bin;
+
+    new_binned_data.push_back(temp);
+  }
+
+  // Write the binned data to a file
+  writeCSV("output_equifreq.csv", new_binned_data);
+
+  cout << "Equi-frequency binning complete." << endl;
 }
 
 int main()
 {
-  vector<int> data = readCSV("input.csv");
-  int m;
+  int n;
 
-  int method;
-  cout << "Choose binning method: " << endl;
-  cout << "1. Equal Frequency Binning" << endl;
-  cout << "2. Equal Width Binning" << endl;
-  cout << "\nEnter method number: ";
-  cin >> method;
-  cout << "\nEnter number of bins: ";
-  cin >> m;
+  while (true)
+  {
+    cout << "Enter 1 for equi width" << endl;
+    cout << "Enter 2 for equi frequency" << endl;
+    cout << "Enter any other value to exit" << endl;
+    cin >> n;
 
-  if (method == 1)
-  {
-    vector<vector<int>> freqBins = equifreq(data, m);
-    writeCSV("output_equifreq.csv", freqBins);
+    if (n == 1)
+    {
+      equiwidthBinning();
+    }
+    else if (n == 2)
+    {
+      equiFrequencyBinning();
+    }
+    else
+    {
+      cout << "Invalid option. Exiting." << endl;
+      break;
+    }
   }
-  else if (method == 2)
-  {
-    vector<vector<int>> widthBins = equiwidth(data, m);
-    writeCSV("output_equiwidth.csv", widthBins);
-  }
-  else
-  {
-    cout << "Invalid method choice." << endl;
-  }
-
-  return 0;
 }
