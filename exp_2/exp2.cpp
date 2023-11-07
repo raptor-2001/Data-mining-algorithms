6dbe6a722ee9cc8c91affdaba79c9841dc5ae280
raptor-2001 <pratham007yadav@gmail.com>
2023-11-07 08:55:09 +0530

    exp2 changed

--- exp_2/exp2.cpp
+++ exp_2/exp2.cpp
@@ -1,122 +1,202 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <climits>
+#include <cmath>
+
 using namespace std;
 
-void calculateMinMax(ifstream &inputFile, double &minValue, double &maxValue)
+// Function to read CSV data from a file and return it as a vector of vectors of strings
+vector<vector<string>> readCSV(const string &filename)
 {
-    double currentValue;
-    inputFile >> currentValue;
-    while (inputFile)
+    vector<vector<string>> data;
+    ifstream file(filename);
+    if (!file.is_open())
     {
-        if (currentValue > maxValue)
-            maxValue = currentValue;
-        if (currentValue < minValue)
-            minValue = currentValue;
-        inputFile >> currentValue;
+        cerr << "Error: File " << filename << " not found." << endl;
+        return data;
     }
+
+    string line;
+    while (getline(file, line))
+    {
+        vector<string> row;
+        istringstream lineStream(line);
+        string cell;
+        while (getline(lineStream, cell, ','))
+        {
+            row.push_back(cell);
+        }
+        data.push_back(row);
+    }
+
+    file.close();
+    return data;
 }
 
-void performMinMaxNormalization(ifstream &inputFile, ofstream &outputFile, double oldMin, double oldMax, double newMin, double newMax)
+// Function to write CSV data to a file
+void writeCSV(const string &filename, const vector<vector<string>> &data)
 {
-    outputFile << "Original Data,"
-               << "Normalized Data"
-               << "\n";
-
-    double currentValue;
-    inputFile >> currentValue;
-    while (inputFile)
+    ofstream file(filename);
+    if (!file.is_open())
     {
-        double previousValue = currentValue;
-        currentValue = (((currentValue - oldMin) / (oldMax - oldMin)) * (newMax - newMin)) + newMin;
-        outputFile << previousValue << "," << currentValue << "\n";
-        inputFile >> currentValue;
+        cerr << "Error: Unable to create or open file " << filename << " for writing." << endl;
+        return;
     }
+
+    for (const auto &row : data)
+    {
+        for (size_t i = 0; i < row.size(); ++i)
+        {
+            file << row[i];
+            if (i < row.size() - 1)
+            {
+                file << ",";
+            }
+        }
+        file << endl;
+    }
+
+    file.close();
 }
 
-void performZScoreNormalization(ifstream &inputFile, ofstream &outputFile)
+// Function for Min-Max normalization
+void minMaxNormalization()
 {
-    double sum = 0.0, count = 0.0, squareSum = 0.0, mean, standardDeviation;
-    double currentValue;
+    // Find the minimum and maximum values in the data
+    const vector<vector<string>> data = readCSV("exp2_input_MinMax.csv");
+    double min_value;
+    double max_value;
 
-    // Calculate mean
-    while (inputFile)
+    double min_data = INT_MAX;
+    double max_data = INT_MIN;
+
+    cout << "Enter min and max values: ";
+    cin >> min_value >> max_value;
+
+    // Calculate the minimum and maximum values in the data
+    for (const vector<string> &row : data)
     {
-        sum += currentValue;
-        count++;
-        inputFile >> currentValue;
+        for (const string &cell : row)
+        {
+            double value = stod(cell);
+            if (value < min_data)
+            {
+                min_data = value;
+            }
+            if (value > max_data)
+            {
+                max_data = value;
+            }
+        }
     }
-    mean = sum / count;
 
-    // Calculate standard deviation
-    inputFile.clear();
-    inputFile.seekg(0, ios::beg);
-    while (inputFile)
+    // Perform Min-Max normalization
+    vector<vector<string>> normalized_data = data;
+
+    for (vector<string> &row : normalized_data)
     {
-        squareSum += (currentValue - mean) * (currentValue - mean);
-        inputFile >> currentValue;
+        for (string &cell : row)
+        {
+            double value = stod(cell);
+            double normalized_value = (value - min_data) / (max_data - min_data) * (max_value - min_value) + min_value;
+            cell = to_string(normalized_value);
+        }
     }
-    inputFile.clear();
-    inputFile.seekg(0, ios::beg);
 
-    standardDeviation = sqrt(squareSum / count);
+    // Write the normalized data to a file
+    writeCSV("exp2_output_MinMax.csv", normalized_data);
 
-    // Perform z-score normalization
-    outputFile << "Original Data,"
-               << "Normalized Data"
-               << "\n";
+    cout << "\n\nNormalization complete successfully.\n\n"
+         << endl;
+}
 
-    while (inputFile)
+// Function for Z-score normalization
+void zScoreNormalization()
+{
+    // Read the input data from a file
+    const vector<vector<string>> data = readCSV("exp2_input_Zscore.csv");
+
+    int size = data[0].size();
+    vector<double> mean(size, 0.0);
+    vector<double> stdDev(size, 0.0);
+
+    // Calculate the mean for each column
+    for (const vector<string> &row : data)
     {
-        double prev = currentValue;
-        currentValue = (currentValue - mean) / standardDeviation;
-        outputFile << prev << "," << currentValue << endl;
-        inputFile >> currentValue;
+        for (size_t i = 0; i < row.size(); ++i)
+        {
+            double value = stod(row[i]);
+            mean[i] += value;
+        }
     }
+
+    for (double &m : mean)
+    {
+        m /= data.size();
+    }
+
+    // Calculate the standard deviation for each column
+    for (const vector<string> &row : data)
+    {
+        for (size_t i = 0; i < row.size(); ++i)
+        {
+            double value = stod(row[i]);
+            stdDev[i] += (value - mean[i]) * (value - mean[i]);
+        }
+    }
+
+    for (double &sd : stdDev)
+    {
+        sd = sqrt(sd / data.size());
+    }
+
+    // Perform Z-score normalization
+    vector<vector<string>> normalized_data = data;
+
+    for (vector<string> &row : normalized_data)
+    {
+        for (size_t i = 0; i < row.size(); ++i)
+        {
+            double value = stod(row[i]);
+            double normalized_value = (value - mean[i]) / stdDev[i];
+            row[i] = to_string(normalized_value);
+        }
+    }
+
+    // Write the normalized data to a file
+    writeCSV("exp2_output_Zscore.csv", normalized_data);
+
+    cout << "\n\nZ-score normalization complete.\n\n"
+         << endl;
 }
 
 int main()
 {
-    double currentValue, minValue, maxValue, newMinValue, newMaxValue;
-    double sum, count, squareSum, mean, standardDeviation;
+    int n;
 
-    ifstream inputFileMinMax("exp2_input_MinMax.csv");
-    ifstream inputFileMinMax_2("exp2_input_MinMax.csv");
-    ifstream inputFileZScore("exp2_input_Zscore.csv");
-
-    int option;
-    cout << "\nEnter an option: \n1. Min-Max Normalization \n2. Z-Score Normalization\nOption: ";
-    cin >> option;
-
-    ofstream outputFileMinMax("exp2_output_MinMax.csv", ios::app);
-    ofstream outputFileZScore("exp2_output_ZScore.csv", ios::app);
-
-    switch (option)
+    while (true)
     {
-    case 1: // Min-Max Normalization
-        if (!inputFileMinMax)
-        {
-            cout << "Error opening file, please try again.";
-            exit(0);
-        }
-        calculateMinMax(inputFileMinMax, minValue, maxValue);
-        cout << "Enter new minimum value: ";
-        cin >> newMinValue;
-        cout << "\nEnter new maximum value: ";
-        cin >> newMaxValue;
-        performMinMaxNormalization(inputFileMinMax_2, outputFileMinMax, minValue, maxValue, newMinValue, newMaxValue);
-        break;
+        cout << "Enter 1 for Min-Max normalization" << endl;
+        cout << "Enter 2 for Z-score normalization" << endl;
+        cout << "Enter any other value to exit" << endl;
+        cin >> n;
 
-    case 2: // Z-Score Normalization
-        if (!inputFileZScore)
+        if (n == 1)
         {
-            cout << "Error opening file, please try again.";
-            exit(0);
+            minMaxNormalization();
         }
-        performZScoreNormalization(inputFileZScore, outputFileZScore);
-        break;
-
-    default:
-        cout << "Invalid option";
+        else if (n == 2)
+        {
+            zScoreNormalization();
+        }
+        else
+        {
+            cout << "Invalid option. Exiting." << endl;
+            break;
+        }
     }
 
     return 0;
